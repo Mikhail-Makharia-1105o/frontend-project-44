f6fe008e4223553c098f409ba7a9912f464b09b2
Mikhail Makharia <makharia.study@gmail.com>
2023-11-27 14:04:00 +0300

    improved calc maintainability

--- bin/games/brain-calc.js
+++ bin/games/brain-calc.js
@@ -10,35 +10,24 @@ export default function gameCalc() {
         const b = Math.floor(Math.random() * 110);
         const c = Math.floor(Math.random() * 110);
         let sign = '';
+        let answer = 0;
         if (c >= 0 && c <= 33) {
-        sign = '+';
+            sign = '+';
+            answer = a + b;
         } else if (c > 33 && c <= 66) {
             sign = '-';
+            answer = a - b;
         } else {
             sign = '*';
+            answer = a * b;
         }
         const userAnswer = readlineSync.question(`Question: ${a} ${sign} ${b} `);
-        let answer = 0;
-        switch (sign) {
-            case '+':
-                answer = a + b;
-                break;
-            case '-':
-                answer = a - b;
-                break;
-            case '*':
-                answer = a * b;
-                break;
-            default:
-                return;
-        }
         if (+userAnswer !== answer) {
             console.log(`'${+userAnswer}' is a wrong answer ;(. Correct answer was '${answer}'.`);
             failState();
             return;
         }
     }
-    winState();
-}
+    winState();}
 
 gameCalc();
