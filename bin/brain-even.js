c45547d3e728d3c2c6393df5111ca28a332f8ec9
Mikhail Makharia <makharia.study@gmail.com>
2023-11-27 13:44:10 +0300

    some more mantainability

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -8,13 +8,9 @@ export default function gameEven() {
   for (let i = 0; i < 3; i += 1) {
     const question = Math.floor((Math.random() * 110)) || 11;
     console.log(`Question: ${question}`);
-    const questionEven1 = readlineSync.question('Your answer: ');
-    if (questionEven1 !== 'no' && (question % 2 !== 0)) {
-      console.log(`${questionEven1} is a wrong answer ;(. Correct answer was 'no'.`);
-      failState();
-      return;
-    } if (questionEven1 !== 'yes' && (question % 2 === 0)) {
-      console.log(`${questionEven1} is a wrong answer ;(. Correct answer was 'yes'.`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer !== 'no' && (question % 2 !== 0) || userAnswer !== 'yes' && (question % 2 === 0)) {
+      console.log(`${userAnswer} is a wrong answer ;(. Correct answer was '${userAnswer === 'yes' ? 'no' : 'yes'}'.`);
       failState();
       return;
     }
