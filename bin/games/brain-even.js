2bdc4b212f54e03acff3672b95622814b1c44f08
Mikhail Makharia <makharia.study@gmail.com>
2023-11-28 11:29:32 +0300

    test adjustements

--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-import { answerCheckingLogic, winState, gameStart } from '../src/index.js';
+import { answerCheckingLogic, winState, gameStart } from '../../src/index.js';
 
 gameStart();
 export default function gameEven() {
