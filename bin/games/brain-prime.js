1fc03d22af61983ce5d92a04716a2ce7f5c260df
Mikhail Makharia <makharia.study@gmail.com>
2023-11-30 14:09:03 +0300

    Fixed primenumber logic

--- bin/games/brain-prime.js
+++ bin/games/brain-prime.js
@@ -6,7 +6,7 @@ import { winState, gameStart, answerCheckingLogic } from '../../src/index.js';
 
 const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 gameStart(rules);
-const primeNumbers = '2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97'.split(
+const primeNumbers = '2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 103'.split(
   ' ',
 );
 for (let i = 0; i < primeNumbers.length; i += 1) {
