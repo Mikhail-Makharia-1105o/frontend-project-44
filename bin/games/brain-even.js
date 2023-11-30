da00115b1cd999063d0aef912b0f632a14af138f
Mikhail Makharia <makharia.study@gmail.com>
2023-11-30 13:32:23 +0300

    Updated pathing to bin

--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -4,12 +4,8 @@ import readlineSync from 'readline-sync';
 
 import { answerCheckingLogic, winState, gameStart } from '../../src/index.js';
 
-//gameStart();
+gameStart();
 export default function gameEven() {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 0; i < 3; i += 1) {
     const question = Math.floor((Math.random() * 110)) || 11;
     console.log(`Question: ${question}`);
